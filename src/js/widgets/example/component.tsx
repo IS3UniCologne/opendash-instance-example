362132a17eb2dc7ba4e9db475af8fb97ee9e4e52
Sebastian Zilles <dev@zilles.io>
2023-11-02 15:33:13 +0100

    Update service import

--- src/js/widgets/example/component.tsx
+++ src/js/widgets/example/component.tsx
@@ -1,20 +1,15 @@
-import React from "react";
-
-import {
-  FormatRelativeDates,
-  useTranslation,
-  useOpenDashServices,
-} from "@opendash/core";
+import { useTranslation } from "@opendash/core";
 
 import { createWidgetComponent } from "@opendash/plugin-monitoring";
 
+import { useDataService } from "@opendash/plugin-timeseries";
 import { ConfigInterface } from "./types";
 
 export default createWidgetComponent<ConfigInterface>(
   ({ config, ...context }) => {
     const t = useTranslation();
 
-    const { DataService } = useOpenDashServices();
+    const DataService = useDataService();
 
     context.setLoading(false);
     context.setName(t("app:widgets.example.title"));
